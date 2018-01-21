8ad550bd63282dc73c65487d7d0c20563da14e1d
Lily <lily@>
2018-01-20 21:02:29 -0500

    updated

--- Print_function.py
+++ Print_function.py
@@ -2,3 +2,18 @@ n = int(input())
 print(*range(1,n+1), sep = '')
 
 # or: print(*range(1, int(input())+1), sep='')
+
+
+#
+# Read an integer .
+#
+# Without using any string methods, try to print the following:
+#
+#
+# Note that "" represents the values in between.
+#
+# Input Format
+# The first line contains an integer .
+#
+# Output Format
+# Output the answer as explained in the task.
