5904c1ee52c9036773ce53429ac5dbe036b9c8ab
lilyyyyyyyyy <hz1464@nyu.edu>
2018-01-31 11:25:52 -0500

    another way to do it

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -1,2 +1,32 @@
 n = int(input().strip())
-print(bin(n)[2:].split('0'))
+print(len(max(bin(n).strip()[2:].split('0'))))
+
+
+# another way for Python3:
+
+n = int(input().strip())
+
+# create an empty list to hold our binary numbers
+binary = []
+
+# fill the list with 1s and 0s from left to right using Base-Conversion Method (remainders of dividing by 2)
+
+while n > 0:
+    remainder = n%2
+    binary.append(remainder)
+    n = n//2
+consec = 0
+maximum = 0
+
+# iterate through our list of 1s and 0s to find our max consecutive set of 1s
+for i in binary:
+    if i == 1:
+        consec += 1
+        if maximum <= consec:
+            maximum = consec
+        else:
+            pass
+    elif i == 0:
+        consec = 0
+
+print(maximum)
